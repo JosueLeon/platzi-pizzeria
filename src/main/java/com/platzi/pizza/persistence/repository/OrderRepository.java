3b7057024f262466d590f9bcfd938ec904d081e3
Josue Leon <josue.leon.canto@gmail.com>
2023-12-06 19:34:15 -0600

    add jquery for order customer

--- src/main/java/com/platzi/pizza/persistence/repository/OrderRepository.java
+++ src/main/java/com/platzi/pizza/persistence/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.platzi.pizza.persistence.repository;
 
 import com.platzi.pizza.persistence.entity.OrderEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,4 +13,8 @@ public interface OrderRepository extends ListCrudRepository<OrderEntity, Integer
     List<OrderEntity> findAllByDateAfter(LocalDateTime date);
 
     List<OrderEntity> findAllByMethodIn(List<String> methods);
+
+    @Query(value = "SELECT * FROM pizza_order WHERE id_customer = :id", nativeQuery = true)
+    List<OrderEntity> findCustomerOrders(@Param("id") String idCustomer);
+
 }
